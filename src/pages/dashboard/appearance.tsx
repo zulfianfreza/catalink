d968e64b5297c495fa4238c197bdb96d2eb934ba
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2023-09-22 10:57:52 +0700

    fix loading state

--- src/pages/dashboard/appearance.tsx
+++ src/pages/dashboard/appearance.tsx
@@ -1,6 +1,7 @@
 import { getSession } from 'next-auth/react'
 import { NextSeo } from 'next-seo'
 import ContentContainer from '~/components/ContentContainer'
+import Loading from '~/components/Loading'
 import Logo from '~/components/Logo'
 import { TabButtons, TabFonts, TabProfile, TabThemes } from '~/components/tab'
 import { DashboardTemplate } from '~/components/template'
@@ -47,26 +48,7 @@ export default function AppearancePage() {
           </TabsList>
           {isLoadingSite ? (
             <div className=' flex h-full w-full items-center justify-center'>
-              <svg
-                className='-ml-1 mr-3 h-5 w-5 animate-spin text-violet-700'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-              >
-                <circle
-                  className='opacity-25'
-                  cx={12}
-                  cy={12}
-                  r={10}
-                  stroke='currentColor'
-                  strokeWidth={4}
-                />
-                <path
-                  className='opacity-75'
-                  fill='currentColor'
-                  d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                />
-              </svg>
+              <Loading />
             </div>
           ) : (
             <TabsContent value='profile'>
@@ -77,15 +59,33 @@ export default function AppearancePage() {
               />
             </TabsContent>
           )}
-          <TabsContent value='themes'>
-            <TabThemes theme={dataTheme} refetch={refetchTheme} />
-          </TabsContent>
-          <TabsContent value='buttons'>
-            <TabButtons theme={dataTheme} refetch={refetchTheme} />
-          </TabsContent>
-          <TabsContent value='fonts'>
-            <TabFonts theme={dataTheme} refetch={refetchTheme} />
-          </TabsContent>
+          {isLoadingTheme ? (
+            <div className=' flex h-full w-full items-center justify-center'>
+              <Loading />
+            </div>
+          ) : (
+            <TabsContent value='themes'>
+              <TabThemes theme={dataTheme} refetch={refetchTheme} />
+            </TabsContent>
+          )}
+          {isLoadingTheme ? (
+            <div className=' flex h-full w-full items-center justify-center'>
+              <Loading />
+            </div>
+          ) : (
+            <TabsContent value='buttons'>
+              <TabButtons theme={dataTheme} refetch={refetchTheme} />
+            </TabsContent>
+          )}
+          {isLoadingTheme ? (
+            <div className=' flex h-full w-full items-center justify-center'>
+              <Loading />
+            </div>
+          ) : (
+            <TabsContent value='fonts'>
+              <TabFonts theme={dataTheme} refetch={refetchTheme} />
+            </TabsContent>
+          )}
         </Tabs>
       </ContentContainer>
     </DashboardTemplate>
